d6a517c2b6e4bf3b39b9c7ec3da3d16af8c171f9
Carmen <cfernando1115@gmail.com>
2021-06-12 17:16:46 -0400

    Basic layout

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,2 +1,32 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+(function () {
+    //Links animation
+    const linkBoxes = document.querySelectorAll('.links-container__link');
+
+    linkBoxes.forEach(linkBox => {
+        linkBox.addEventListener('mouseenter', displayLink);
+    });
+
+    function displayLink(e) {
+        const link = e.target.querySelector('p');
+        link.classList.remove('hidden');
+        e.target.removeEventListener('mouseenter', displayLink);
+    }
+
+    //Links eventlistener
+    const linksContainer = document.querySelector('.links-container');
+    linksContainer.addEventListener('click', (e) => {
+        const clicked = e.target.closest('.links-container__link a');
+
+        if (!clicked) {
+            return;
+        }
+
+        document.querySelector(clicked.getAttribute('href'))
+            .scrollIntoView({ behavior: "smooth" });
+    })
+})();
+
+
